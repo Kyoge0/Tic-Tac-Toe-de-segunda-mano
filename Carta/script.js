0d9fec7a47da6d5c2d175d4d9f0ccbe4b72dae2f
Kyoge0 <94868625+Kyoge0@users.noreply.github.com>
2025-02-12 19:26:24 -0600

    Delete Carta directory

--- Carta/script.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  document.querySelector(".flowers").innerHTML += `
-    <!-- Aquí puedes agregar más flores o animaciones si es necesario -->
-  `;
-});
